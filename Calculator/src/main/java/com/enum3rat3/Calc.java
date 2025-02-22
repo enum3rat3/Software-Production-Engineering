524be8f80c1b7d689d713b3ab488bbf41df31dd5
enum3rat3 <jaiminjadvani3728@gmail.com>
2025-02-22 14:20:20 +0530

    Subtraction, Division, Multiplication operation implemented and pom.xml file updated

--- Calculator/src/main/java/com/enum3rat3/Calc.java
+++ Calculator/src/main/java/com/enum3rat3/Calc.java
@@ -9,25 +9,53 @@ public class Calc {
         {
             System.out.println("------------- Calculator -------------");
             System.out.println("1. Addition");
-            System.out.println("2. Exit");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
 
             Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter which operation you want to perform: ");
+            int op = scanner.nextInt();
+
+            if(op == 5)
+            {
+                System.out.println("Exiting....");
+                break;
+            }
+
             System.out.print("Enter 1st Operand: ");
             double a = scanner.nextDouble();
 
             System.out.print("Enter 2nd Operand: ");
             double b = scanner.nextDouble();
 
-            System.out.print("Enter which operation you want to perform: ");
-            int op = scanner.nextInt();
-
             if(op == 1)
             {
                 System.out.println("Addition of " + a + " + " + b + " = " + (a + b));
             }
+            else if(op == 2)
+            {
+                System.out.println("Subtraction of " + a + " - " + b + " = " + (a - b));
+            }
+            else if (op == 3)
+            {
+                System.out.println("Multiplication of " + a + " * " + b + " = " + Math.round(a * b * 100.0) / 100.0);
+            }
+            else if (op == 4)
+            {
+                if(b != 0)
+                {
+                    System.out.println("Division of " + a + " / " + b + " = " + Math.round((a / b) * 100.0) / 100.0);
+                }
+                else
+                {
+                    System.out.println("Invalid Input");
+                }
+            }
             else
             {
-                System.out.println("Exiting....");
+                System.out.println("Invalid Option....");
                 break;
             }
         }
